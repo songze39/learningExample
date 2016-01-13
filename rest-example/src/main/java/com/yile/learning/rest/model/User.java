30b1e1e60284d70742f9b17f2f5aee58012c768b
justinliang <28554768@qq.com>
2016-01-13 12:15:10 +0800

    model

---
+++ rest-example/src/main/java/com/yile/learning/rest/model/User.java
@@ -0,0 +1,22 @@
+package com.yile.learning.rest.model;
+
+public class User {
+	private String id;
+	private String name;
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
