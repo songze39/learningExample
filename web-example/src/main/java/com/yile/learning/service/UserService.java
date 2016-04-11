d3564f37a3820d585693962001c6eb0afecae5f1
liangjy <28554768@qq.com>
2016-04-11 23:53:38 +0800

    update

---
+++ web-example/src/main/java/com/yile/learning/service/UserService.java
@@ -0,0 +1,5 @@
+package com.yile.learning.service;
+
+public interface UserService {
+
+}
---
+++ web-example/src/main/java/com/yile/learning/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.yile.learning.service.impl;
+
+import com.yile.learning.service.UserService;
+
+public class UserServiceImpl implements UserService {
+	
+}
