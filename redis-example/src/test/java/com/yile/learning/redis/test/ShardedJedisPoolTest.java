bdf0fff514dc4c750df7fd4634230b425b633ddf
liangjy <28554768@qq.com>
2016-04-15 21:27:55 +0800

    update

--- redis-example/src/test/java/com/yile/learning/redis/test/ShardedJedisPoolTest.java
+++ redis-example/src/test/java/com/yile/learning/redis/test/ShardedJedisPoolTest.java
@@ -4,20 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import redis.clients.jedis.JedisShardInfo;
 import redis.clients.jedis.ShardedJedis;
 import redis.clients.jedis.ShardedJedisPool;
 
 public class ShardedJedisPoolTest {
+	private static final Logger logger = LogManager.getLogger(ShardedJedisPoolTest.class);
 	private ShardedJedisPool shardedJedisPool;
 
 	@Before
 	public void before() {
 		List<JedisShardInfo> shards = new ArrayList<>();
-		JedisShardInfo jedisShardInfo = new JedisShardInfo("192.168.134.135", 7002);
+		JedisShardInfo jedisShardInfo = new JedisShardInfo("192.168.2.111", 6379);
 		shards.add(jedisShardInfo);
 		GenericObjectPoolConfig config = new GenericObjectPoolConfig();
 		config.setMaxTotal(1);
@@ -30,6 +34,13 @@ public class ShardedJedisPoolTest {
 	@After
 	public void after() {
 		shardedJedisPool.close();
+	}
+
+	@Test
+	public void setValue() {
+		ShardedJedis shardedJedis = getShardedJedis();
+		String result = shardedJedis.set("redisexample", "shardedjedispool");
+		logger.debug("result:" + result);
 	}
 
 	/**
